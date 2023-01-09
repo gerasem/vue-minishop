d5d8db79fe47f5a45107d31b533301a7c7ee5d11
Dmitri Laptev <dmitri.laptev@sidecaps.com>
2023-01-09 23:39:27 +0100

    get all items from server

---
+++ src/components/vItem.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="item">
+      <router-link to="/">
+        <div class="item__image-container">
+          <img
+            :src="item.image ?? previewImage"
+            :alt="item.title"
+            class="img-fluid item__image"
+          />
+        </div>
+      </router-link>
+
+      <div class="item__description">
+        <div class="item__price">{{ item.price }}€</div>
+        <div
+          class="item__old-price"
+          v-if="item.old_price"
+        >
+          {{ item.old_price }}€
+        </div>
+        <div
+          class="item__discount"
+          v-if="getSale"
+        >
+          {{ getSale }}%
+        </div>
+        <ui-icon
+          icon="bag-plus"
+          @click="addToCart()"
+        >
+        </ui-icon>
+      </div>
+      <h3 class="item__title">{{ item.title }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "vItem",
+
+  data() {
+    return {
+      previewImage: "@/assets/images/320x240.png",
+    };
+  },
+
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    getSale() {
+      if (!this.item.old_price) return;
+      return ((this.item.price / this.item.old_price - 1) * 100).toFixed(0);
+    },
+  },
+
+  methods: {
+    addToCart() {
+      this.$toast.add({
+        summary: "Item added to shopping cart",
+        life: 3000,
+        group: "cart",
+      });
+    },
+  },
+
+  unmounted() {
+    this.$toast.removeGroup("cart");
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.item {
+  text-decoration: none;
+  margin: 25px 0 15px;
+  display: block;
+  color: $color-text;
+
+  &__image-container {
+    overflow: hidden;
+  }
+
+  &__image {
+    width: 100%;
+    clip-path: $clip-path;
+    transition: 0.2s;
+
+    &:hover {
+      transform: scale(1.05);
+    }
+  }
+
+  &__description {
+    display: flex;
+    flex-direction: row;
+    margin: 12px 15px 0;
+    font-size: 1rem;
+    justify-content: space-between;
+  }
+
+  &__price {
+    font-weight: 700;
+    font-size: 1.125rem;
+  }
+
+  &__old-price {
+    text-decoration: line-through;
+  }
+
+  &__discount {
+    color: $color-primary;
+  }
+
+  &__title {
+    margin: 0 15px;
+  }
+}
+</style>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,17 +1,27 @@
 <script setup>
 import { storeToRefs } from "pinia";
 import { useItemsStore } from "@/store/items";
-
+import vItem from "@/components/vItem.vue";
 const { loading, search, items, serverError } = storeToRefs(useItemsStore());
 const { fetchAll } = useItemsStore();
-
 fetchAll();
 </script>
 
 <template>
-  <p>Main page</p>
   <p v-if="serverError">{{ serverError }}</p>
-  <pre>
-    {{ items }}
-  </pre>
+  <main class="container-fluid flex-grow-1">
+    <h1>Main</h1>
+
+    <div class="row">
+      <template
+        v-for="item in items"
+        :key="item.id"
+      >
+        <v-item :item="item"></v-item>
+      </template>
+      <template v-if="!items.length">
+        <div class="col">Items not found</div>
+      </template>
+    </div>
+  </main>
 </template>
