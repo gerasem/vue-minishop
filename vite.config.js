111a8248569bb53dcce85f9dfdfd989b8a22d610
Dmitri Laptev <dmitri.laptev@sidecaps.com>
2023-03-13 00:21:14 +0100

    edited publicPath

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       reactivityTransform: true,
     }),
   ],
-  base: "./",
+  base: "/vue-minishop/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
