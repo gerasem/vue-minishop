d65366d9846b2f219f3dc4fb551a5f885d5b7995
Dmitri Laptev <dmitri.laptev@sidecaps.com>
2023-03-13 12:52:54 +0100

    remove a deprecated feature from config file

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      reactivityTransform: true,
-    }),
-  ],
+  plugins: [vue()],
   base: "/vue-minishop/",
   resolve: {
     alias: {
